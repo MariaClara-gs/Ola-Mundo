6205767928d4d59849a7c8bfdc8fa4fd9e8ec018
Maria Clara <mariaclara2016085@gmail.com>
2022-09-24 14:45:01 -0300

    Funcionando

--- music.py
+++ music.py
@@ -6,13 +6,13 @@ n = randint(0,10)
 sair = ''
 
 
-print ("\n\033[1;35mOlá, Mundo!\033[m")
+print ("\nOlá, Mundo!")
 
 while sair != 's':
      
   if n in numeros:
     if len(numeros) == 11:
-      sys.exit("\n\033[1;31mAcabaram as músicas :P\033[m")
+      sys.exit("\nAcabaram as músicas :P")
     else:
       n = randint(0,10)
    
@@ -20,70 +20,70 @@ while sair != 's':
   else:   
     if n == 0:
       numeros.append(n)
-      print("\n\033[1;36mTocando: Close To You\033[m")
+      print("\nTocando: Close To You\033[m")
       playsound('close.mp3')
       
       
     if n == 1:
       numeros.append(n)
-      print("\n\033[1;36mTocando: Honey\033[m")
+      print("\nTocando: Honey")
       playsound('honey.mp3')
       
 
     if n == 2:
       numeros.append(n)
-      print("\n\033[1;36mTocando: 7 Rings\033[m")
+      print("\nTocando: 7 Rings")
       playsound('7-rings.mp3')
      
 
     if n == 3:
       numeros.append(n)
-      print("\n\033[1;36mTocando: Death Bed\033[m")
+      print("\nTocando: Death Bed")
       playsound('powfu.mp3')
       
 
     if n == 4:
       numeros.append(n)
-      print("\n\033[1;36mTocando: Phobia\033[m")
+      print("\nTocando: Phobia")
       playsound('phobia.mp3')
       
 
     if n == 5:
       numeros.append(n)
-      print("\n\033[1;36mTocando: I Love You Boy\033[m")
+      print("\nTocando: I Love You Boy")
       playsound('i.mp3')
       
     
     if n == 6:
       numeros.append(n)
-      print("\n\033[1;36mTocando: Dream in a Dream\033[m")
+      print("\nTocando: Dream in a Dream")
       playsound('dream.mp3')
       
 
     if n == 7:
       numeros.append(n)
-      print("\n\033[1;36mTocando: Shower\033[m")
+      print("\nTocando: Shower")
       playsound('shower.mp3')
       
 
     if n == 8:
       numeros.append(n)
-      print("\n\033[1;36mTocando: Lovers\033[m")
+      print("\nTocando: Lovers")
       playsound('lovers.mp3')
       
 
     if n == 9:
       numeros.append(n)
-      print("\n\033[1;36mTocando: The Ocean\033[m")
+      print("\nTocando: The Ocean")
       playsound('theocean.mp3')
 
 
     if n == 10:
       numeros.append(n)
-      print("\n\033[1;36mTocando: Dimple\033[m")
+      print("\nTocando: Dimple")
       playsound('dimple.mp3')
 
-    sair = input(str("\n\033[1;36mDeseja sair?\n Pressione S para sair ou O para continuar."))
+    sair = input(str("\nDeseja sair?\n Pressione S para sair ou O para continuar."))
     n = randint(0,10)
 
    
